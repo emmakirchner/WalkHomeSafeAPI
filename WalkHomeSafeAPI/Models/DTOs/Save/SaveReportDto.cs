322bcbe13c1bc07739c13adc46acc1398c3a4239
Emma Kirchner <emma.kirchner@develappers.de>
2026-06-08 07:16:10 +0200

    remove username from SaveReportDto

--- WalkHomeSafeAPI/Models/DTOs/Save/SaveReportDto.cs
+++ WalkHomeSafeAPI/Models/DTOs/Save/SaveReportDto.cs
@@ -2,8 +2,6 @@
 
 public class SaveReportDto
 {
-    public string UserName { get; init; } = string.Empty;
-
     public string Title { get; init; } = string.Empty;
 
     public string Description { get; init; } = string.Empty;
